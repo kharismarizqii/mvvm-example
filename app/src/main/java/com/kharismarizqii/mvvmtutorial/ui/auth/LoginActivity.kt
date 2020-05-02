6c55674ca0b9659b3d978d459d0c365f1d52b52d
kharismarizqii <rizqikharisma@gmail.com>
2020-05-02 12:38:28 +0700

    Injection using Kodein Framework

--- app/src/main/java/com/kharismarizqii/mvvmtutorial/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/kharismarizqii/mvvmtutorial/ui/auth/LoginActivity.kt
@@ -1,40 +1,36 @@
 package com.kharismarizqii.mvvmtutorial.ui.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.kharismarizqii.mvvmtutorial.R
-import com.kharismarizqii.mvvmtutorial.data.db.AppDatabase
 import com.kharismarizqii.mvvmtutorial.data.db.entities.User
-import com.kharismarizqii.mvvmtutorial.data.network.MyApi
-import com.kharismarizqii.mvvmtutorial.data.network.NetworkConnectionInterceptor
-import com.kharismarizqii.mvvmtutorial.data.repository.UserRepository
 import com.kharismarizqii.mvvmtutorial.databinding.ActivityLoginBinding
 import com.kharismarizqii.mvvmtutorial.ui.home.HomeActivity
 import com.kharismarizqii.mvvmtutorial.util.hide
 import com.kharismarizqii.mvvmtutorial.util.show
 import com.kharismarizqii.mvvmtutorial.util.snackbar
-import com.kharismarizqii.mvvmtutorial.util.toast
 import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity(), AuthListener {
+class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+
+    private val factory : AuthViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
-        val api = MyApi(networkConnectionInterceptor)
-        val db = AppDatabase(this)
-        val repository = UserRepository(api,db)
-        val factory = AuthViewModelFactory(repository)
+
 
         val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this, factory).get(AuthViewModel::class.java)
         binding.viewmodel = viewModel
 
         viewModel.authListener = this
